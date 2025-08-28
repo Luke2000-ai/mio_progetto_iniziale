8afae8b90acf64843c3383501152e637258ba059
Luca A <lucandreozzi2000@gmail.com>
2025-08-28 16:30:52 +0200

    invio con nuova cartella

---
+++ Gestione_sconti_dinamici/Prenotazione_task1/Concerto.java
@@ -0,0 +1,17 @@
+public class Concerto extends Prenotazione{
+     String nomeConcerto;
+
+    public Concerto(String nome, int date, int postiRichiesti, String  nomeConcerto ) {
+        super(nome, date, postiRichiesti);
+        this.nomeConcerto = nomeConcerto;
+    }
+
+//@Override
+
+
+
+
+
+
+
+}
---
+++ Gestione_sconti_dinamici/Prenotazione_task1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+    Prenotazione p1= new Prenotazione("Luca", 12042025, 0, 0, true);
+    
+        p1.stampaDettagli();
+        p1.verificaPrenotazione();
+
+
+}
+}
