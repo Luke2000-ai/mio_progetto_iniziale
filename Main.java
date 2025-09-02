b8c46e0a28214c6a7d291894bc3b80c3adbf6d29
Danilo D <dalmatronic@gmail.com>
2025-09-02 11:57:19 +0200

    TAG V1.0

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+    Libro l1= new Libro("Il Signore degli Anelli","Tolkien", 1930);
+    
+        l1.stampaDettagli();
+        
+
+
+}
+}
